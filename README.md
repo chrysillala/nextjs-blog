64490d6276be0e0d417aed726c11e73b55117c2f
chrysilla mayasari <chrysilla_mysr@hotmail.com>
2021-05-20 13:17:39 +0700

    pre-rendering and data fetching

--- README.md
+++ README.md
@@ -9,3 +9,15 @@ This is a starter template for [Learn Next.js](https://nextjs.org/learn).
   - Fast Refresh preserves React local state in function components (and Hooks) by default.
 - Global CSS
   - You can only add global css on `pages/_app.js` because global css affects all elements on the page
+- Pre-rendering
+  - It improves SEO and makes your app able to work even without JS
+  - By default, Next.js pre-renders every page into static HTML. This means that Next.js generates HTML for each page in advance, instead of having it all done by client-side JavaScript.
+  - When a page is loaded, javascript code that is necessary to the page will run and makes the page interactive. This process is called hydration.
+  - There are 2 forms of pre-rendering:
+    - Static Generation: generates HTML at build time. The pre-rendered HTML then reused on each request. This can be done with and without data.
+    - Server-side Rendering: generates HTML on each request
+  - Static Generation with Data using `getStaticProps`. By doing this, we are telling Next.js that this page has some data dependencies. It runs on build time.
+    - `getStaticProps` only runs on server-side. It won't even included in the JS bundle for the browser.
+  - Use `getServerSideProps` to fetch data at request time
+  - If we do not need to pre-render the data, we can use a strategy called client-side rendering. Here, we statically generate the parts of the page that do not need external data. Then when the page loads, fetch the data from client using JS. This approach works well for user dashboard site, for example.
+    - It is recommended to use SWR, a react hook for data fetching when fetching data on client side
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,23 @@ import Head from 'next/head'
 import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData, getEndpointData, getSingleData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  const apiData = await getEndpointData();
+  const singleData = await JSON.parse(getSingleData());
+
+  return {
+    props: {
+      allPostsData,
+      apiData,
+      singleData
+    }
+  }
+}
+
+export default function Home({allPostsData, apiData, singleData}) {
   return (
     <div className="container">
       <Layout home>
@@ -19,6 +34,32 @@ export default function Home() {
           <p>
             (This is a sample website - you’ll be building a site like this on{' '}
             <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          </p>
+        </section>
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({id, date, title}) => (
+              <li className={utilStyles.listItem} key={id}>
+                {title} <br />
+                {date} <br />
+                {id}
+              </li>
+            ))}
+          </ul>
+          <ul>
+            {apiData.map(({id, name, email}) => (
+              <li key={id}>
+                {name} - {email.toLowerCase()}
+              </li>
+            ))}
+          </ul>
+        </section>
+        <section>
+          <p>
+            { singleData.id } <br />
+            <strong>{ singleData.title } </strong><br />
+            { singleData.body }
           </p>
         </section>
       </Layout>
